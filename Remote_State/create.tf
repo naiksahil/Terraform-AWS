f4d06c6eea34f4d506e49c8bcf49e6ce8aca10f9
Naik <sahil-harishchandra.naik@capgemini.com>
2026-05-04 23:16:47 +0530

    Updated Creat.tf

--- Remote_State/create.tf
+++ Remote_State/create.tf
@@ -1,14 +1,34 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "demo_sg" {
+  name        = "demo-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  required_version = ">= 1.2.0"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-provider "aws" {
-  region  = "us-east-1"
-}
+
+
