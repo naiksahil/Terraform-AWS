49a9199b90e350414de611c7a0be0baf9790bcc9
naiksahil <53610613+naiksahil@users.noreply.github.com>
2026-08-02 17:44:42 +0530

    Update Backend.tf

--- Remote_State/Backend.tf
+++ Remote_State/Backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "884348118743-terraform-states"
     key            = "remote_state/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-3"
     use_lockfile   = true
     encrypt        = true
   }
